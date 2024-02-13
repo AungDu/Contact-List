db90c7560021a2f80c8167c94b73cfd7708b4d8b
AungDu <92lamai@gmail.com>
2024-02-12 19:31:37 -0500

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+
+
 import ContactList from './components/ContactList'
+import { useState, useEffect } from 'react'
+import ContactDetails from './components/ContactDetails';
+import './App.css'
 
 
 
 function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
+  const [detailedContact, setDetailedContact] = useState(null);
+
+  useEffect(() => {
+    async function fetchSelectedContact() {
+      try {
+        const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/" + selectedContactId);
+        const result = await response.json();
+        setDetailedContact(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (selectedContactId) {
+      fetchSelectedContact()
+    }
+  }, [selectedContactId])
+
 
   return (
     <>
-      {selectedContactId ? (
-        <div>Selected Contact View</div>
-      ) : (
-        <ContactList />
-       
-      )}
+      {detailedContact ? (<ContactDetails detailedContact={detailedContact}/>) : (<ContactList setSelectedContactId={setSelectedContactId} />)}
     </>
-  );
+  )
 }
 export default App
--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,10 +1,7 @@
-export default function ContactRow({ setSelectedContactId, contact }) {
-    return (
-      <tr
-        onClick={() => {
-          setSelectedContactId(contact.id);
-        }}
-      >
+export default function ContactRow({ contact, setSelectedContactId }) {
+    
+  return (
+      <tr onClick={() => setSelectedContactId(contact.id)}>
         <td>{contact.name}</td>
         <td>{contact.email}</td>
         <td>{contact.phone}</td>
